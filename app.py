e437f995fd5aca9000a181e189c67086af98ca35
Manera <mediabihunsipitung@gmail.com>
2026-07-09 15:24:30 +0700

    Fix passenger_wsgi

--- app.py
+++ app.py
@@ -29,7 +29,16 @@ def mre():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
-   if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Flask"
