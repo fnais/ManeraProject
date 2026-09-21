e0f2ca20b1fcba161631c5bfcc00dbebfd6c88e4
Manera <mediabihunsipitung@gmail.com>
2026-09-21 11:35:45 +0700

    Update Senin

--- app.py
+++ app.py
@@ -2,32 +2,57 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# =====================================================
+# HOME
+# =====================================================
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+# =====================================================
+# PRODUK
+# =====================================================
+
 @app.route('/Bihun_Jagung_Pion_Tunggal')
-def Bihun_Jagung_Pion_Tunggal():
+def bihun_jagung_pion_tunggal():
     return render_template('Bihun_Jagung_Pion_Tunggal.html')
 
+
 @app.route('/Bihun_Jagung_Pion_Tunggal_140gr')
-def Bihun_Jagung_Pion_Tunggal_140gr():
+def bihun_jagung_pion_tunggal_140gr():
     return render_template('Bihun_Jagung_Pion_Tunggal_140gr.html')
 
 
 @app.route('/Bihun_Jagung_Pitung')
-def granola():
+def bihun_jagung_pitung():
     return render_template('Bihun_Jagung_Pitung.html')
 
 
 @app.route('/Sohun_Iyon')
-def saus():
+def sohun_iyon():
     return render_template('Sohun_Iyon.html')
 
 
 @app.route('/Penyedap_Rasa_Sisuka')
-def mre():
+def penyedap_rasa_sisuka():
     return render_template('Penyedap_Rasa_Sisuka.html')
 
-if __name__ == "__main__":
+
+# =====================================================
+# KATALOG
+# =====================================================
+
+@app.route('/Katalog')
+def katalog():
+    return render_template('Katalog.html')
+
+
+# =====================================================
+# RUN SERVER
+# =====================================================
+
+if __name__ == '__main__':
     app.run(debug=True)
